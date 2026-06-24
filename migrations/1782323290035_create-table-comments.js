64086c62ab77b943bdb6daa19b146e7ec8ed72b7
denidoodle12 <denih500@gmail.com>
2026-06-25 00:49:53 +0700

    feat: add database migrations for threads, comments, and replies tables

---
+++ migrations/1782323290035_create-table-comments.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"threads"',
+      onDelete: 'CASCADE',
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"users"',
+      onDelete: 'CASCADE',
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    is_delete: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: false,
+    },
+    date: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('comments');
+};
---
+++ migrations/1782323291934_create-table-replies.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('replies', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    comment_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"comments"',
+      onDelete: 'CASCADE',
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"users"',
+      onDelete: 'CASCADE',
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    is_delete: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: false,
+    },
+    date: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('replies');
+};
