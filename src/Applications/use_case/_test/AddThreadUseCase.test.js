76e2d21fd2b696122369dacbff239e6b02985bac
denidoodle12 <denih500@gmail.com>
2026-06-25 00:58:06 +0700

    feat: add AddThreadUseCase and its tests

---
+++ src/Applications/use_case/_test/AddThreadUseCase.test.js
@@ -0,0 +1,49 @@
+import { vi } from 'vitest';
+import CreateThread from '../../../Domains/threads/entities/CreateThread.js';
+import CreatedThread from '../../../Domains/threads/entities/CreatedThread.js';
+import ThreadRepository from '../../../Domains/threads/ThreadRepository.js';
+import AddThreadUseCase from '../AddThreadUseCase.js';
+
+describe('AddThreadUseCase', () => {
+  it('should orchestrating the add thread action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      title: 'dicoding',
+      body: 'secret',
+      owner: 'user-123',
+    };
+
+    const mockCreatedThread = new CreatedThread({
+      id: 'thread-123',
+      title: useCasePayload.title,
+      owner: useCasePayload.owner,
+    });
+
+    /** creating dependency of use case */
+    const mockThreadRepository = new ThreadRepository();
+
+    /** mocking needed function */
+    mockThreadRepository.addThread = vi.fn()
+      .mockImplementation(() => Promise.resolve(mockCreatedThread));
+
+    /** creating use case instance */
+    const addThreadUseCase = new AddThreadUseCase({
+      threadRepository: mockThreadRepository,
+    });
+
+    // Action
+    const createdThread = await addThreadUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(createdThread).toStrictEqual(new CreatedThread({
+      id: 'thread-123',
+      title: useCasePayload.title,
+      owner: useCasePayload.owner,
+    }));
+    expect(mockThreadRepository.addThread).toBeCalledWith(new CreateThread({
+      title: useCasePayload.title,
+      body: useCasePayload.body,
+      owner: useCasePayload.owner,
+    }));
+  });
+});
