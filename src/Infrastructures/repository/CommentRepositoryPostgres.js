2c342720e6a8e2ff32502deb37d8fa3ab6849237
denidoodle12 <denih500@gmail.com>
2026-06-25 01:08:13 +0700

    feat: add postgres repository implementations and their integration tests

---
+++ src/Infrastructures/repository/CommentRepositoryPostgres.js
@@ -0,0 +1,82 @@
+import CommentRepository from '../../Domains/comments/CommentRepository.js';
+import CreatedComment from '../../Domains/comments/entities/CreatedComment.js';
+import NotFoundError from '../../Commons/exceptions/NotFoundError.js';
+import AuthorizationError from '../../Commons/exceptions/AuthorizationError.js';
+
+class CommentRepositoryPostgres extends CommentRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async addComment(createComment) {
+    const { threadId, content, owner } = createComment;
+    const id = `comment-${this._idGenerator()}`;
+    const isDelete = false;
+    const date = new Date().toISOString();
+
+    const query = {
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5, $6) RETURNING id, content, owner',
+      values: [id, threadId, owner, content, isDelete, date],
+    };
+
+    const result = await this._pool.query(query);
+    return new CreatedComment({ ...result.rows[0] });
+  }
+
+  async verifyCommentExists(commentId) {
+    const query = {
+      text: 'SELECT id FROM comments WHERE id = $1',
+      values: [commentId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError('komentar tidak ditemukan');
+    }
+  }
+
+  async verifyCommentOwner(commentId, owner) {
+    const query = {
+      text: 'SELECT owner FROM comments WHERE id = $1',
+      values: [commentId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError('komentar tidak ditemukan');
+    }
+
+    if (result.rows[0].owner !== owner) {
+      throw new AuthorizationError('anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async deleteComment(commentId) {
+    const query = {
+      text: 'UPDATE comments SET is_delete = true WHERE id = $1',
+      values: [commentId],
+    };
+
+    await this._pool.query(query);
+  }
+
+  async getCommentsByThreadId(threadId) {
+    const query = {
+      text: `SELECT comments.id, users.username, comments.date, comments.content, comments.is_delete 
+             FROM comments 
+             JOIN users ON comments.owner = users.id 
+             WHERE comments.thread_id = $1 
+             ORDER BY comments.date ASC`,
+      values: [threadId],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
+}
+
+export default CommentRepositoryPostgres;
